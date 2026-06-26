a28b86ea59861b0001db4bfc3a2b39da76158020
Bikash kr yadav <bikasyadav73@gmail.com>
2026-06-26 17:09:38 +0545

    Time: 4 ms (22.47%) | Memory: 23.6 MB (11.48%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word;
+        string result="";
+        while(ss>>word){
+            if (result.empty()) {
+                result = word; 
+            } else {
+                result = word + " " + result; 
+            }
+        }
+        return result;
+    }
+};
