7e3ab5bfc9135d50cfe5e117a85b3fa5f7b1adbb
Bikash kr yadav <bikasyadav73@gmail.com>
2026-07-29 20:33:36 +0530

    Time: 3 ms (86.24%) | Memory: 20.8 MB (42.83%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,43 @@
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+private:
+    int totalPaths = 0;
+    unordered_map<long long, int> prefixSumMap;
+
+    void dfs(TreeNode* node, long long currentSum, int targetSum) {
+        if (!node) return;
+
+     
+        currentSum += node->val;
+
+       
+        if (prefixSumMap.count(currentSum - targetSum)) {
+            totalPaths += prefixSumMap[currentSum - targetSum];
+        }
+
+      
+        prefixSumMap[currentSum]++;
+
+        
+        dfs(node->left, currentSum, targetSum);
+        dfs(node->right, currentSum, targetSum);
+
+        
+        prefixSumMap[currentSum]--;
+    }
+
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        totalPaths = 0;
+        prefixSumMap.clear();
+        
+        
+        prefixSumMap[0] = 1;
+        
+        dfs(root, 0, targetSum);
+        return totalPaths;
+    }
+};
