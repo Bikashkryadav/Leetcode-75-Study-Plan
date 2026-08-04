fb472ea6ea5a8dcfa0b73849fe650a6f6971505a
Bikash kr yadav <bikasyadav73@gmail.com>
2026-08-04 22:57:34 +0530

    Time: 8 ms (12.18%) | Memory: 33.6 MB (36.53%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        auto [min_it, max_it] = std::minmax_element(nums.begin(), nums.end());
+        int min_val = *min_it;
+        int max_val = *max_it;
+        
+       
+        std::unordered_set<int> num_set(nums.begin(), nums.end());
+        std::vector<int> missing;
+        
+       
+        for (int i = min_val; i <= max_val; ++i) {
+            if (num_set.find(i) == num_set.end()) {
+                missing.push_back(i);
+            }
+        }
+        
+        return missing;
+    }
+};
